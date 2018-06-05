bd9942ed90feddf37ac2dbc8dfb4b502d80dee40
swankgd <swankgd@gmail.com>
2018-06-04 17:44:20 -0700

    v0.2.0 group and user creation

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,3 +5,13 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 package 'java-1.7.0-openjdk-devel'
+
+group 'tomcat'
+	
+user 'tomcat' do
+	shell '/sbin/nologin'
+	manage_home false
+	git 'tomcat'
+	home '/opt/tompcat'
+end
+
